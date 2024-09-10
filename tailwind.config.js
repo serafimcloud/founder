ba8f4ea159581c9381a02b107b0a09c8c478b8a4
serafim.eth <serafimcloud@gmail.com>
2024-09-10 12:56:30 +0300

    Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
-        bg: "#000000",
-        text: "#ffffff",
-        rowBg: "var(--gas-row-bg, #2525258C)",
-        label: "#ebebf599",
-        main: "var(--tg-theme-accent-text-color, #499FDF)",
-        shareLinkBg: "#000000CC",
-        shareLinkText: "#EBEBF599",
-        buy: "#2FBD85",
-        sell: "#F54660",
-        white48: "rgba(255,255,255,0.48)",
-        white24: "rgba(255,255,255,0.24)",
-        white8: "rgba(255,255,255,0.08)",
-        darkBlue100: "#007AFF",
-        modal: "#1C1C1E",
-        border: "#FFFFFF14",
-        label30: "#ebebf54d",
-        label16: "#ebebf529",
-        addImageBg: "#212B32",
-        dedust: "#EC502C",
-      },
-      animation: {
-        blink: "blink 0.25s linear",
-      },
-      keyframes: {
-        blink: {
-          "0%": { background: "white" },
-        },
+        bg_color: 'var(--tg-theme-bg-color, #FFFFFF)',
+        text_color: 'var(--tg-theme-text-color, #000000)',
+        hint_color: 'var(--tg-theme-hint-color, #8E8E93)',
+        link_color: 'var(--tg-theme-link-color, #007AFF)',
+        button_color: 'var(--tg-theme-button-color, #007AFF)',
+        secondary_button_color: 'var(--tg-theme-secondary-button-color, #FFFFFF70)',
+        button_text_color: 'var(--tg-theme-button-text-color, #FFFFFF)',
+        secondary_bg_color: 'var(--tg-theme-secondary-bg-color, #EFEFF4)',
+        header_bg_color: 'var(--tg-theme-header-bg-color, #EFEFF3)',
+        accent_text_color: 'var(--tg-theme-accent-text-color, #000000)',
+        section_bg_color: 'var(--tg-theme-section-bg-color, #FFFFFF)',
+        section_header_text_color: 'var(--tg-theme-section-header-text-color, #000000)',
+        section_separator_color: 'var(--tg-theme-section-separator-color, #CCCCCC)',
+        subtitle_text_color: 'var(--tg-theme-subtitle-text-color, #888888)',
+        destructive_text_color: 'var(--tg-theme-destructive-text-color, #FF0000)',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [
+    function({ addBase }) {
+      addBase({
+        ':root': {
+          '--tg-color-scheme': 'light',
+        },
+        '@media (prefers-color-scheme: dark)': {
+          ':root': {
+            '--tg-color-scheme': 'dark',
+          },
+        },
+        'body[data-theme="dark"]': {
+          '--tg-color-scheme': 'dark',
+        },
+        'body[data-theme="light"]': {
+          '--tg-color-scheme': 'light',
+        },
+      });
+    },
+  ],
+}
